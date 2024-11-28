5f226c276d8d5bd37b9c0e97f4911638010b4b51
Ruchir-BridgeLabz <ruchir.dixit@bridgelabz.com>
2024-11-28 23:26:25 +0530

    First Setup project commit

---
+++ NewsAggregatorProject/src/main/java/com/airtribe/app/NewsAggregatorProjectApplication.java
@@ -0,0 +1,13 @@
+package com.airtribe.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAggregatorProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAggregatorProjectApplication.class, args);
+	}
+
+}
